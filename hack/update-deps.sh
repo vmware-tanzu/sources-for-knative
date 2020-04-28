45e85976ec28725044e094c3597f8198bdd52a1e
Ville Aikas <vaikas@vmware.com>
2020-04-28 08:19:26 -0700

    fix the typos

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -22,9 +22,9 @@ export GOFLAGS=-mod=vendor
 # The list of dependencies that we track at HEAD and periodically
 # float forward in this repository.
 FLOATING_DEPS=(
-  "knative.dev/pkg@{$VERSION}"
-  "knative.dev/eventing@{$VERSION}"
-  "knative.dev/test-infra@{$VERSION}"
+  "knative.dev/pkg@${VERSION}"
+  "knative.dev/eventing@${VERSION}"
+  "knative.dev/test-infra@${VERSION}"
 )
 
 # Parse flags to determine any we should pass to dep.
