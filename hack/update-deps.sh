73081b6052b062bb019beb2564ffe020f5abc64e
Scott Nichols <snichols@vmware.com>
2020-09-29 17:05:45 -0700

    use buoy to help manage releases for knative and k8s

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -12,35 +12,46 @@ set -o pipefail
 
 cd ${ROOT_DIR}
 
-# This controls the release branch we track.
-# VERSION="release-0.17" # use this one for pre-release week.
-VERSION="master"         # use this one for development times.
+# This controls the release we track.
+VERSION="v0.19"
 
+# This controls the Kubernetes release we track.
+K8S_VERSION="v0.18"
 
 # We need these flags for things to work properly.
 export GO111MODULE=on
 
-# The list of dependencies that we track at HEAD and periodically
-# float forward in this repository.
-FLOATING_DEPS=(
-  "knative.dev/test-infra@${VERSION}"
-  "knative.dev/pkg@${VERSION}"
-  "knative.dev/eventing@${VERSION}"
-)
-
 # Parse flags to determine any we should pass to dep.
-GO_GET=0
+UPGRADE=0
 while [[ $# -ne 0 ]]; do
   parameter=$1
   case ${parameter} in
-    --upgrade) GO_GET=1 ;;
+    --upgrade) UPGRADE=1 ;;
+    --release) shift; VERSION="$1";;
+    --k8s-release) shift; K8S_VERSION="$1";;
     *) abort "unknown option ${parameter}" ;;
   esac
   shift
 done
-readonly GO_GET
+readonly UPGRADE
+readonly VERSION
 
-if (( GO_GET )); then
+#     -require="${dep}@${K8S_VERSION}" \
+#    -replace="${dep}=${dep}@${K8S_VERSION}"
+
+if (( UPGRADE )); then
+  K8S_DEPS=( $(run_go_tool tableflip.dev/buoy buoy float ${ROOT_DIR}/go.mod --domain k8s.io --release ${K8S_VERSION} --strict) )
+  echo "Floating k8s deps to ${K8S_DEPS[@]}"
+  for dep in "${K8S_DEPS[@]}"
+  do
+    # note, ${dep%@*} strips the the string after the '@', i.e.: k8s.io/code-generator@v0.18.10 => k8s.io/code-generator
+    go mod edit -replace="${dep%@*}=${dep}"
+  done
+fi
+
+if (( UPGRADE )); then
+    FLOATING_DEPS=( $(run_go_tool tableflip.dev/buoy buoy float ${ROOT_DIR}/go.mod --release ${VERSION}) )
+    echo "Floating deps to ${FLOATING_DEPS[@]}"
     go get -d ${FLOATING_DEPS[@]}
 fi
 
