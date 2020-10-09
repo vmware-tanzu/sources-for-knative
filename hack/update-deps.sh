eb831c8749cc5032c7f56ce021e5069c9e513cdb
Scott Nichols <snichols@vmware.com>
2020-10-09 14:20:24 -0700

    use buoy to help manage releases for knative (#98)
    * use buoy to help manage releases for knative and k8s
    
    * leverage test-infra go_update_deps

--- hack/update-deps.sh
+++ hack/update-deps.sh
@@ -3,50 +3,10 @@
 # Copyright 2020 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-readonly ROOT_DIR=$(dirname $0)/..
-source ${ROOT_DIR}/vendor/knative.dev/test-infra/scripts/library.sh
-
 set -o errexit
 set -o nounset
 set -o pipefail
 
-cd ${ROOT_DIR}
+source $(dirname "$0")/../vendor/knative.dev/test-infra/scripts/library.sh
 
-# This controls the release branch we track.
-# VERSION="release-0.17" # use this one for pre-release week.
-VERSION="master"         # use this one for development times.
-
-
-# We need these flags for things to work properly.
-export GO111MODULE=on
-
-# The list of dependencies that we track at HEAD and periodically
-# float forward in this repository.
-FLOATING_DEPS=(
-  "knative.dev/test-infra@${VERSION}"
-  "knative.dev/pkg@${VERSION}"
-  "knative.dev/eventing@${VERSION}"
-)
-
-# Parse flags to determine any we should pass to dep.
-GO_GET=0
-while [[ $# -ne 0 ]]; do
-  parameter=$1
-  case ${parameter} in
-    --upgrade) GO_GET=1 ;;
-    *) abort "unknown option ${parameter}" ;;
-  esac
-  shift
-done
-readonly GO_GET
-
-if (( GO_GET )); then
-    go get -d ${FLOATING_DEPS[@]}
-fi
-
-# Prune modules.
-go mod tidy
-go mod vendor
-
-rm -rf $(find vendor/ -name 'OWNERS')
-rm -rf $(find vendor/ -name '*_test.go')
+go_update_deps "$@"
