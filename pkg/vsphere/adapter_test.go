6fb3c296ce1567ba20d576153afd22844af25bb0
Ville Aikas <vaikas@vmware.com>
2020-11-04 13:07:06 +0200

    add one failing, one success test

--- pkg/vsphere/adapter_test.go
+++ pkg/vsphere/adapter_test.go
@@ -7,25 +7,27 @@ package vsphere
 
 import (
 	"context"
+	"errors"
 	"net/http"
 	"testing"
 	"time"
 
+	cloudevents "github.com/cloudevents/sdk-go/v2"
+	"github.com/cloudevents/sdk-go/v2/binding"
 	"github.com/cloudevents/sdk-go/v2/client"
 	cecontext "github.com/cloudevents/sdk-go/v2/context"
+	"github.com/cloudevents/sdk-go/v2/event"
+	cehttp "github.com/cloudevents/sdk-go/v2/protocol/http"
+	"github.com/google/go-cmp/cmp"
 	"github.com/vmware/govmomi/vim25/types"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zaptest"
-
-	"github.com/cloudevents/sdk-go/v2/binding"
-	"github.com/cloudevents/sdk-go/v2/event"
-	cehttp "github.com/cloudevents/sdk-go/v2/protocol/http"
 )
 
 type roundTripperTest struct {
 	statusCodes  []int
 	requestCount int
-	events       []event.Event
+	events       []*event.Event
 }
 
 func (r *roundTripperTest) RoundTrip(req *http.Request) (*http.Response, error) {
@@ -35,7 +37,7 @@ func (r *roundTripperTest) RoundTrip(req *http.Request) (*http.Response, error) 
 	if err != nil {
 		return nil, err
 	}
-	r.events = append(r.events, *e)
+	r.events = append(r.events, e)
 	return &http.Response{StatusCode: code}, nil
 }
 
@@ -48,17 +50,27 @@ func (m *mockType) GetEvent() *types.Event {
 }
 
 func TestSendEvents(t *testing.T) {
+	now := time.Now()
+	baseEvent := &mockType{event: &types.Event{CreatedTime: now}}
 	testCases := map[string]struct {
 		statusCodes []int
 		moref       types.ManagedObjectReference
 		baseEvents  []types.BaseEvent
-		wantEvents  []event.Event
+		wantEvents  []*event.Event
 		result      error
 	}{
 		"one event, succeeds": {
 			statusCodes: []int{200},
 			moref:       types.ManagedObjectReference{Value: "VirtualMachine", Type: "vm59"},
-			baseEvents:  []types.BaseEvent{&mockType{event: &types.Event{CreatedTime: time.Now()}}},
+			baseEvents:  []types.BaseEvent{baseEvent},
+			wantEvents:  []*event.Event{createEvent("test-source", "mockType", "0", "eventex", nil, baseEvent, now)},
+		},
+		"one event, fails": {
+			statusCodes: []int{500},
+			moref:       types.ManagedObjectReference{Value: "VirtualMachine", Type: "vm59"},
+			baseEvents:  []types.BaseEvent{&mockType{event: &types.Event{CreatedTime: now}}},
+			wantEvents:  []*event.Event{createEvent("test-source", "mockType", "0", "eventex", nil, baseEvent, now)},
+			result:      errors.New("500: "),
 		},
 	}
 	for n, tc := range testCases {
@@ -82,9 +94,36 @@ func TestSendEvents(t *testing.T) {
 			adapter := vAdapter{Logger: logger.Sugar(), CEClient: c, Source: "test-source"}
 			sendEvents := adapter.sendEvents(ctx)
 			sendResult := sendEvents(tc.moref, tc.baseEvents)
-			if sendResult != tc.result {
-				t.Errorf("Failed to send events, expected %v got %v", tc.result, sendResult)
+			if tc.result == nil && sendResult != nil {
+				t.Error("Unexpected result from sendEvents, wanted no error got ", sendResult)
+			} else if tc.result != nil && sendResult == nil {
+				t.Error("Unexpected result from sendEvents, did not get expected error ", tc.result)
+			} else if tc.result != nil && sendResult != nil {
+				if sendResult.Error() != tc.result.Error() {
+					t.Errorf("Unexpected result from sendEvents, expected %v got %v", tc.result, sendResult)
+				}
+			}
+			for i := range tc.wantEvents {
+				if diff := cmp.Diff(tc.wantEvents[i], roundTripper.events[i]); diff != "" {
+					t.Error("unexpected diff in events", diff)
+				}
 			}
 		})
 	}
+}
+
+func createEvent(eventSource, eventType, eventID, extensionKey string, extensionValue interface{}, baseEvent interface{}, eventTime time.Time) *event.Event {
+	ev := cloudevents.NewEvent(cloudevents.VersionV1)
+
+	ev.SetType("com.vmware.vsphere." + eventType)
+	ev.SetTime(eventTime)
+	ev.SetID(eventID)
+	ev.SetSource(eventSource)
+	if extensionKey != "" {
+		ev.SetExtension(extensionKey, extensionValue)
+	}
+	if err := ev.SetData("application/xml", baseEvent); err != nil {
+		panic("Failed to SetData")
+	}
+	return &ev
 }
