cfb16845548d7fefe0f0a847a001aead34cb4d52
knative-automation <automation@knative.team>
2021-01-18 23:13:13 -0800

    upgrade to latest dependencies (#172)
    bumping knative.dev/eventing 1c94ef8...d395f6f:
      > d395f6f Cleanup channel template internal types (# 4749)
      > 8e0e286 :minidisc: Change KIND's k8s to 1.19.4 and add 1.20.0 (# 4729)
      > 0f2f567 Format markdown (# 4746)
    bumping knative.dev/pkg f0ea5e6...75d66b5:
      > 75d66b5 preserve the stable webhook logger names we had before (# 1994)
      > 22f4898 upgrade to latest dependencies (# 1990)
      > 5bb97df fix duration logging (# 1992)
      > a74906c Use structured logging to augment our logger vs. naming (# 1991)
    
    Signed-off-by: Knative Automation <automation@knative.team>

--- go.mod
+++ go.mod
@@ -28,9 +28,9 @@ require (
 	k8s.io/client-go v11.0.1-0.20190805182717-6502b5e7b1b5+incompatible
 	k8s.io/code-generator v0.18.12
 	k8s.io/kube-openapi v0.0.0-20200410145947-bcb3869e6f29
-	knative.dev/eventing v0.20.1-0.20210115015220-1c94ef83b6b5
+	knative.dev/eventing v0.20.1-0.20210118112921-d395f6f13a8f
 	knative.dev/hack v0.0.0-20210114150620-4422dcadb3c8
-	knative.dev/pkg v0.0.0-20210114223020-f0ea5e6b9c4e
+	knative.dev/pkg v0.0.0-20210118192521-75d66b58948d
 )
 
 replace (
--- pkg/client/injection/reconciler/sources/v1alpha1/vspheresource/controller.go
+++ pkg/client/injection/reconciler/sources/v1alpha1/vspheresource/controller.go
@@ -16,6 +16,7 @@ import (
 	versionedscheme "github.com/vmware-tanzu/sources-for-knative/pkg/client/clientset/versioned/scheme"
 	client "github.com/vmware-tanzu/sources-for-knative/pkg/client/injection/client"
 	vspheresource "github.com/vmware-tanzu/sources-for-knative/pkg/client/injection/informers/sources/v1alpha1/vspheresource"
+	zap "go.uber.org/zap"
 	corev1 "k8s.io/api/core/v1"
 	labels "k8s.io/apimachinery/pkg/labels"
 	types "k8s.io/apimachinery/pkg/types"
@@ -26,6 +27,7 @@ import (
 	kubeclient "knative.dev/pkg/client/injection/kube/client"
 	controller "knative.dev/pkg/controller"
 	logging "knative.dev/pkg/logging"
+	logkey "knative.dev/pkg/logging/logkey"
 	reconciler "knative.dev/pkg/reconciler"
 )
 
@@ -73,10 +75,16 @@ func NewImpl(ctx context.Context, r Interface, optionsFns ...controller.OptionsF
 		finalizerName: defaultFinalizerName,
 	}
 
-	t := reflect.TypeOf(r).Elem()
-	queueName := fmt.Sprintf("%s.%s", strings.ReplaceAll(t.PkgPath(), "/", "-"), t.Name())
+	ctrType := reflect.TypeOf(r).Elem()
+	ctrTypeName := fmt.Sprintf("%s.%s", ctrType.PkgPath(), ctrType.Name())
+	ctrTypeName = strings.ReplaceAll(ctrTypeName, "/", ".")
 
-	impl := controller.NewImpl(rec, logger, queueName)
+	logger = logger.With(
+		zap.String(logkey.ControllerType, ctrTypeName),
+		zap.String(logkey.Kind, "sources.tanzu.vmware.com.VSphereSource"),
+	)
+
+	impl := controller.NewImpl(rec, logger, ctrTypeName)
 	agentName := defaultControllerAgentName
 
 	// Pass impl to the options. Save any optional results.
