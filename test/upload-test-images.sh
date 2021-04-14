8cc514edf8980e316792344cb13d55720832a945
Michael Gasch <mgasch@vmware.com>
2021-04-14 02:23:23 +0200

    Fix shellcheck issues (#235)
    Closes: #234
    Signed-off-by: Michael Gasch <mgasch@vmware.com>

--- test/upload-test-images.sh
+++ test/upload-test-images.sh
@@ -3,12 +3,12 @@
 # Copyright 2020 VMware, Inc.
 # SPDX-License-Identifier: Apache-2.0
 
-: ${KO_DOCKER_REPO:?"You must set 'KO_DOCKER_REPO', see DEVELOPMENT.md"}
+: "${KO_DOCKER_REPO:?"You must set 'KO_DOCKER_REPO', see DEVELOPMENT.md"}"
 
 export GO111MODULE=on
 export GOFLAGS=-mod=vendor
 
-cat | ko resolve -Bf - <<EOF
+cat << EOF| ko resolve -Bf -
 images:
 - ko://github.com/vmware/govmomi/govc
 - ko://github.com/vmware-tanzu/sources-for-knative/test/test_images/listener
