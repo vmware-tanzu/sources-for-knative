5dce05d2b0f630b7bcf8aa3603d4b7e4973aa272
Clay Kauzlaric <ckauzlaric@vmware.com>
2022-04-07 11:52:25 -0400

    wip: address comments

--- test/test_images/listener/main.go
+++ test/test_images/listener/main.go
@@ -9,7 +9,6 @@ import (
 	"context"
 	"encoding/xml"
 	"log"
-	"strconv"
 
 	cloudevents "github.com/cloudevents/sdk-go/v2"
 	"github.com/kelseyhightower/envconfig"
@@ -24,7 +23,7 @@ const (
 
 type envConfig struct {
 	ExpectedEventType  string `envconfig:"EVENT_TYPE"`
-	ExpectedEventCount string `envconfig:"EVENT_COUNT"`
+	ExpectedEventCount int    `envconfig:"EVENT_COUNT"`
 }
 
 type VMPoweredOffEvent struct {
@@ -33,36 +32,35 @@ type VMPoweredOffEvent struct {
 }
 
 func main() {
-	ctx := signals.NewContext()
+	var env envConfig
+	if err := envconfig.Process("", &env); err != nil {
+		log.Fatalf("Unable to read environment config: %v", err)
+	}
+
+	numExpectedEvents := env.ExpectedEventCount
 
 	client, err := cloudevents.NewClientHTTP()
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
+	ctx := signals.NewContext()
+
 	ctx, cancel := context.WithCancel(ctx)
 	defer cancel()
 
-	var env envConfig
-	if err := envconfig.Process("", &env); err != nil {
-		log.Fatalf("Unable to read environment config: %v", err)
-	}
-
-	numExpectedEvents, err := strconv.Atoi(env.ExpectedEventCount)
-	if err != nil {
-		log.Fatalf("Invalid value for EVENT_COUNT (%v): %v", env.ExpectedEventCount, err)
-	}
+	log := logging.FromContext(ctx)
 
 	events := make(chan cloudevents.Event, numExpectedEvents)
 	// receive events, putting them into the channel only if they meet the type we are expecting
 	go func() {
 		if err = client.StartReceiver(ctx, func(event cloudevents.Event) {
-			logging.FromContext(ctx).Infof("Received event: %s", event.String())
+			log.Infof("Received event: %s", event.String())
 			if event.Type() == env.ExpectedEventType {
 				events <- event
 			}
 		}); err != nil {
-			logging.FromContext(ctx).Fatalf("receiving events: %v", err)
+			log.Fatalf("receiving events: %v", err)
 		}
 	}()
 
@@ -71,29 +69,26 @@ func main() {
 	for event := range events {
 		eventData := &VMPoweredOffEvent{}
 		if err := xml.Unmarshal(event.Data(), eventData); err != nil {
-			logging.FromContext(ctx).Fatalf("Failed to unmarshal CloudEvent xml data: ", err)
+			log.Fatalf("Failed to unmarshal CloudEvent xml data: ", err)
 		}
-		logging.FromContext(ctx).Infof("Raw Message: %s", eventData.Message)
+		log.Infof("Raw Message: %s", eventData.Message)
 
 		// assert required CE extension attributes are always present
 		if event.Extensions()[ceVSphereEventClass] == "" {
-			logging.FromContext(ctx).Fatalf("CloudEvent extension %q not set", ceVSphereEventClass)
+			log.Fatalf("CloudEvent extension %q not set", ceVSphereEventClass)
 		}
 		if event.Extensions()[ceVSphereAPIKey] == "" {
-			logging.FromContext(ctx).Fatalf("CloudEvent extension %q not set", ceVSphereAPIKey)
+			log.Fatalf("CloudEvent extension %q not set", ceVSphereAPIKey)
 		}
 
 		count += 1
-
 		if count == numExpectedEvents {
-			logging.FromContext(ctx).Infow("cancelling context: Received expected number of events")
+			log.Infow("cancelling context: Received expected number of events")
 			cancel()
 			break
 		}
+
 	}
 
-	if count == 0 {
-		logging.FromContext(ctx).Fatalf("no events received")
-	}
-	logging.FromContext(ctx).Infow("successfully received event(s)", "count", count)
+	log.Infow("successfully received event(s)", "count", count)
 }
